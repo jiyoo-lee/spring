cd587b4862491408f17d75e38dc3aca5b28761b7
Jiyoo lee <103879030+jiyoo-lee@users.noreply.github.com>
2022-09-06 17:38:36 +0900

    Add files via upload

--- test3/src/main/java/test3/select4.java
+++ test3/src/main/java/test3/select4.java
@@ -12,7 +12,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;	
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;	
 
 @Controller
 public class select4 {
@@ -20,23 +22,97 @@ public class select4 {
 	@Inject
 	private SqlSessionFactory sqlSessionFactory;
 	
-	@Resource(name = "Xmljava")
+	@Resource(name = "XmlJava")
 	private XmlJava xmljava;
 	
 	Connection connection = null;
 	PreparedStatement pr = null;
+	
+	
+	@RequestMapping("/deleteuser")
+	public String deleteInfo (Model model, String mid, String usernm, String mpw) {
+		
+		try {
+			SqlSession session = sqlSessionFactory.openSession();
+			System.out.println("접-성");
+			
+			String delete = xmljava.deleteInfo(mid,usernm,mpw);
+			
+			if(delete != "ok") {
+				model.addAttribute("result", "delete denied");
+			}
+			else {
+				model.addAttribute("result","delete success");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "view/delete2";
+	}
+	
+	
+	
+	
+	@RequestMapping("/member_update")
+	public String updateInfo (Model model, String mid, String usernm, String mpw) {
+		try {
+			SqlSession session = sqlSessionFactory.openSession();
+			System.out.println("DB 접속 성공");
+			String update = xmljava.updateInfo(mid, usernm, mpw);
+			
+			if(update != "ok") {
+				model.addAttribute("result", "update denied");
+			}
+			else {
+				model.addAttribute("result", "update success");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "view/update2";
+	}
+	
+	
+	
+	@RequestMapping("/select5.do")
+	public String insertmb (Model model, String mid, String mname, String mpw, String mtel, String mage) {
+		
+		try {
+			SqlSession session = sqlSessionFactory.openSession();
+			System.out.println("db연결성공");
+			String call = xmljava.userinsert(mid, mname, mpw, mtel, mage);
+						
+			if( call != "ok") {
+				model.addAttribute("result", "insert denied");
+			}
+			else {
+				model.addAttribute("result", "insert success");
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "view/input";
+	}
+	
+
 	
 	@RequestMapping("/select4.do")
 	public String homepage(Model model) {
 		
 		try {
 			SqlSession session = sqlSessionFactory.openSession();
-			List<uservo> datas = xmljava.selectAll();
+			System.out.println("db연결성공");
+			List<uservo> datas = xmljava.selectall();
+			
 			System.out.println(datas.size()+"명 입니다.");
 			
-			uservo uv = xmljava.selectId("hong");
-			System.out.println("사용자 이름" + uv.getUsernm());
-			System.out.println("사용자 아아디" + uv.getMid());
+			List<uservo> test = xmljava.selectage(10);
+			
+			model.addAttribute("test",test);
+			
 			
 		}catch (Exception e) {
 			e.printStackTrace();
--- test3/src/main/webapp/WEB-INF/view/select4.jsp
+++ test3/src/main/webapp/WEB-INF/view/select4.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@taglib prefix="z" uri="http://java.sun.com/jsp/jstl/core" %>  
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,14 @@
 <title>select4.java와 연계 lombok으로 jdbc 연결</title>
 </head>
 <body>
-
+<div style="border:1px solid lightgray; width: 500px; height: auto; text-align: left;">
+<h1>Included DATA</h1>
+<z:forEach  items ="${test}" var ="test">
+ID : ${test.getId()} <br>
+사용자 ID : ${test.getMid()} <br>
+사용자명 : ${test.getUsernm()} <br>
+사용자 나이 : ${test.getMage()} <br><br>
+</z:forEach>
+</div>
 </body>
 </html>
